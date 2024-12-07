eced5d5cd398204c47fa0cd0a93a1a7f50e68417
kolinhuang0427 <kolinhuang0428@gmail.com>
2024-12-07 12:15:06 -0500

    1

--- MPItest.py
+++ MPItest.py
@@ -2,3 +2,5 @@ from mpi4py import MPI
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()  # Process rank (0 or 1 in our case)
 size = comm.Get_size()  # Total number of processes
+
+print(f"Hello from rank {rank}, total number of processes: {size}")
