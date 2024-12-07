7722ec4bbd5bf791fdd63612b27e82c544c94127
kolinhuang0427 <kolinhuang0428@gmail.com>
2024-12-07 12:43:45 -0500

    1

--- MPItest.py
+++ MPItest.py
@@ -415,10 +415,10 @@ optimizer.load_state_dict(torch.load(optimizer_checkpoint_path, weights_only=Tru
 
 args = {
     'C': 2,
-    'num_searches': 2,
-    'num_iterations': 2,
-    'num_selfPlay_iterations': 4,
-    'num_parallel_games': 2,
+    'num_searches': 800,
+    'num_iterations': 20,
+    'num_selfPlay_iterations': 500,
+    'num_parallel_games': 125,
     'num_epochs': 4,
     'batch_size': 128,
     'temperature': 1.25,
